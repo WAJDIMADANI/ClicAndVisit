59a1996385094617215a7f8aca8c946f7590167b
sazouziExt <sazouzi.ext@deltadore.com>
2023-06-07 11:22:31 +0100

    Add layout views

--- app/src/main/java/com/clickandvisit/ui/user/profile/ProfileViewModel.kt
+++ app/src/main/java/com/clickandvisit/ui/user/profile/ProfileViewModel.kt
@@ -1,12 +1,22 @@
 package com.clickandvisit.ui.user.profile
 
 import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
 import com.clickandvisit.base.BaseAndroidViewModel
 import com.clickandvisit.data.repository.abs.UserRepository
+import com.clickandvisit.global.helper.ImagePicker
 import com.clickandvisit.global.helper.Navigation
 import com.clickandvisit.global.listener.SchedulerProvider
 import com.clickandvisit.global.listener.ToolBarListener
+import com.clickandvisit.global.utils.DebugLog
+import com.clickandvisit.global.utils.TAG
+import com.clickandvisit.ui.shared.dialog.ImgPickerDialog
+import com.clickandvisit.ui.user.signup.PROFILE_PIC_NAME
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,12 +28,96 @@ class ProfileViewModel
 ) : BaseAndroidViewModel(application, schedulerProvider), ToolBarListener {
 
 
+    val userName: MutableLiveData<String> = MutableLiveData()
+    val cin: MutableLiveData<String> = MutableLiveData()
+    val email: MutableLiveData<String> = MutableLiveData()
+    val phone: MutableLiveData<String> = MutableLiveData()
+
+    val userFieldError: MutableLiveData<Boolean> = MutableLiveData(false)
+    val cinFieldError: MutableLiveData<Boolean> = MutableLiveData(false)
+    val emailFieldError: MutableLiveData<Boolean> = MutableLiveData(false)
+    val phoneFieldError: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    val imagePickerDialog: MutableLiveData<ImgPickerDialog> = MutableLiveData()
+
+    val photoUri = MutableLiveData(Uri.EMPTY)
+
+
     init {
 
     }
 
+
+    fun onImageClick() {
+        imagePickerDialog.value =
+            ImgPickerDialog.build(
+                onTakePictureClicked(),
+                onPickPictureClicked(),
+                onDismissBottomSheet()
+            )
+    }
+
+    fun onChangePasswordClicked() {
+
+    }
+
+
+    private fun onTakePictureClicked(): () -> Unit {
+        return {
+            navigate(Navigation.CameraNavigation(com.clickandvisit.ui.user.signup.PROFILE_PIC_NAME))
+        }
+    }
+
+    fun cameraPermissionGranted() {
+        navigate(Navigation.CameraNavigation(com.clickandvisit.ui.user.signup.PROFILE_PIC_NAME))
+    }
+
+    private fun onPickPictureClicked(): () -> Unit {
+        return {
+            navigate(Navigation.GalleryNavigation)
+        }
+    }
+
+    private fun onDismissBottomSheet(): () -> Unit {
+        return {
+            imagePickerDialog.value = null
+        }
+    }
+
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        viewModelScope.launch {
+            val picture = withContext(schedulerProvider.dispatchersIO()) {
+                val picture: Uri? = when (requestCode) {
+                    ImagePicker.PICK_IMAGE_CAMERA_ID -> ImagePicker.getUriFromResult(
+                        applicationContext,
+                        resultCode,
+                        data,
+                        true,
+                        PROFILE_PIC_NAME
+                    )
+                    ImagePicker.PICK_IMAGE_GALLERY_ID -> ImagePicker.getUriFromResult(
+                        applicationContext,
+                        resultCode,
+                        data,
+                        false,
+                        PROFILE_PIC_NAME
+                    )
+                    else -> Uri.EMPTY
+                }
+                picture
+            }
+            photoUri.value = picture
+            DebugLog.i(TAG, picture.toString())
+        }
+    }
+
     fun onBackClick() {
         navigate(Navigation.Back)
+    }
+
+
+    fun onSaveClicked() {
+
     }
 
 }
