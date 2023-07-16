cdde0d7e47034c3c5a4e7d4d78dfb74e4a7f0bf9
sazouziExt <sazouzi.ext@deltadore.com>
2023-07-16 19:18:54 +0100

    Fix bugs

--- app/src/main/java/com/clickandvisit/ui/user/signin/SignInActivity.kt
+++ app/src/main/java/com/clickandvisit/ui/user/signin/SignInActivity.kt
@@ -2,6 +2,7 @@ package com.clickandvisit.ui.user.signin
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.clickandvisit.R
@@ -27,6 +28,7 @@ class SignInActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_signin)
 
@@ -34,6 +36,17 @@ class SignInActivity : BaseActivity() {
         registerSinInObservers()
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }
 
     private fun registerSinInObservers() {
 
